fc80a8fa40f90befb42819fbf6c8d8b6907af9d9
Aleksey Kurepin <lesha@kurepin.com>
2014-02-17 00:24:41 +0400

    deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,10 +5,12 @@ set :user, 'foxweb'
 set :deploy_to, '/home/foxweb/www/rediron.ru'
 set :repository, 'git@github.com:foxweb/rediron.ru.git'
 set :branch, 'master'
+set :keep_releases, 5
 
 desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
     invoke :'git:clone'
+    invoke :'deploy:cleanup'
   end
 end
